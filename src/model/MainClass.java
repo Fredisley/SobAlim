885533fbfd66cd4655f0013b5c3f410d45940897
Fredisley <fredih849@gmail.com>
2024-09-15 13:35:44 -0400

    Nuevos controladores 09-15-24

--- src/model/MainClass.java
+++ src/model/MainClass.java
@@ -1,18 +1,18 @@
+
 package model;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class MainClass extends Application {
-    
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/VentanaInicio.fxml"));
         
         Scene scene = new Scene(root);
         
@@ -23,4 +23,6 @@ public class MainClass extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    
 }
+
